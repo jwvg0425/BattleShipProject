2cec9ba6851675dbc26fc89981f165e58dee9346
jwvg0425 <jwvg0425@naver.com>
2014-07-09 19:00:08 +0900

    20140709
    input&ouput

--- BattleShip_남현욱/BattleShip_PP/BattleShip_PP.cpp
+++ BattleShip_남현욱/BattleShip_PP/BattleShip_PP.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <vector>
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	argc;
+	argv;
+
+	char buffer[256] = { 0, };
+	char* output[] = { "miss", "hit", "destroy" };
+
+	scanf_s("%s", buffer);
+
+	srand((unsigned int)time(NULL));
+
+	printf_s("%s", output[rand() % (sizeof(output) / sizeof(output[0]))]);
+
+	fflush(stdin);
+	getchar();
+
 	return 0;
 }
 
