096c254467e1a61014ca4e2fba7c078bf5ad1f2c
jwvg0425 <jwvg0425@naver.com>
2014-07-16 18:15:07 +0900

    0716
    0716

--- BattleShip_남현욱/BattleShip_PP/BattleShip_PP.cpp
+++ BattleShip_남현욱/BattleShip_PP/BattleShip_PP.cpp
@@ -2,31 +2,60 @@
 //
 
 #include "stdafx.h"
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 
+const char* strDestroy = "Destroy";
+const char* strHit = "Hit";
+const char* strMiss = "Miss";
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain()
 {
-	argc;
-	argv;
-
-	char buffer[256] = { 0, };
-	char* output[] = { "miss", "hit", "destroy" };
-
-	scanf_s("%s", buffer);
+	std::string strinput;
+	std::string strResult[] = { strHit, strMiss, strDestroy };
+	int kind, i;
+	int destroyedKind;
+	std::list<std::string> ships;
+	std::list<std::string> destroyedShips;
+	std::list<std::string> ::iterator it;
+	ships.push_back("Aircraft");
+	ships.push_back("Battleship");
+	ships.push_back("Cruiser");
+	ships.push_back("Destroyer");
+	ships.push_back("Destroyer");
+	ships.push_back("Destroyer");
 
 	srand((unsigned int)time(NULL));
 
-	printf_s("%s", output[rand() % (sizeof(output) / sizeof(output[0]))]);
+	while (!ships.empty())
+	{
+		std::cin >> strinput;
+		kind = rand() % _countof(strResult);
+		std::cout << strResult[kind];
+		if (strResult[kind] == strDestroy)
+		{
+			destroyedKind = rand() % ships.size();
+			std::cout << " - ";
+			
+			for (i = 0, it = ships.begin(); i < destroyedKind; i++, it++);
 
+			destroyedShips.push_back(*it);
+			ships.erase(it);
+
+			for (it = destroyedShips.begin(); it != destroyedShips. end(); it++)
+			{
+				std::cout << *it << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "GAME OVER!" << std::endl;
 	fflush(stdin);
 	getchar();
 
 	return 0;
 }
-
